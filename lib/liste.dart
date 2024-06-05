8b7870f7baa5ab3d6cfc9ce4bcd953b18da8ebe7
Shahrzadzhr <sh.zahrayi@gmail.com>
2024-06-05 15:55:25 +0200

    aktualisieren

---
+++ lib/liste.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class ItemListScreen extends StatefulWidget {
+  const ItemListScreen({super.key});
+
+  @override
+  _ItemListScreenState createState() => _ItemListScreenState();
+}
+
+class _ItemListScreenState extends State<ItemListScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<String> _items = [];
+
+  void _addItem() {
+    if (_textController.text.isNotEmpty) {
+      setState(() {
+        _items.add(_textController.text);
+        _textController.clear();
+      });
+    }
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _items.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Liste"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: "Insert text",
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _textController.clear();
+                  },
+                  icon: const Icon(Icons.clear),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _addItem,
+            child: const Text("Add"),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_items[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _removeItem(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
